3597c1b5f6a5fb6c3a942890df0a7a303bdeb2cc
Stephen <stephencurryliu@163.com>
2024-01-04 14:38:09 +0800

    feat: init

---
+++ src/test/kotlin/org/stephen/reactive/SpringReactiveApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.stephen.reactive
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringReactiveApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
