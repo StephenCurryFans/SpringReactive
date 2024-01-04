1aee9829f19c5c70ba8d8a303ce9d591cbec0aa5
Stephen <stephencurryliu@163.com>
2024-01-04 15:59:36 +0800

    feat: controller test

--- src/main/kotlin/org/stephen/reactive/controller/HelloController.kt
+++ src/main/kotlin/org/stephen/reactive/controller/HelloController.kt
@@ -1,9 +1,17 @@
 package org.stephen.reactive.controller
 
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import org.stephen.reactive.entity.User
+import org.stephen.reactive.repo.UserRepo
 
 @RestController
 @RequestMapping("/hello")
-class HelloController {
+class HelloController(private val userRepo: UserRepo) {
+
+
+    @PostMapping("/save")
+    fun saveUser(user: User) = userRepo.save(user)
+
 }
